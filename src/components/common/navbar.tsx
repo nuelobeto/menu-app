43fd0eed8b1003044ac7ceb0563d20a1ac5ec653
nuelobeto <nuelobeto@gmail.com>
2024-09-11 10:07:01 +0100

    added logo

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -14,13 +14,17 @@ type Props = {
 export const Navbar = ({ goBack, title }: Props) => {
 	const { menu_details } = useStore();
 	const [menuName, setMenuName] = useState("");
+	const [menuImage, setMenuImage] = useState<string | null>(null);
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		if (menu_details) {
 			setMenuName(menu_details.menu_name);
+			setMenuImage(menu_details.menu_image);
 		}
 	}, [menu_details]);
+
+	console.log(menuImage);
 
 	return (
 		<nav className="w-full h-16">
@@ -35,9 +39,12 @@ export const Navbar = ({ goBack, title }: Props) => {
 							<ArrowLeftIcon className="stroke-neutral-400" />
 						</Button>
 					)}
-					<h1 className="text-2xl font-bold text-emerald-50">
-						{title ? title : menuName}
-					</h1>
+					<div className="flex items-center gap-2">
+						{menuImage && <img src={menuImage} alt="" width={30} height={30} />}
+						<h1 className="text-2xl font-bold text-white">
+							{title ? title : menuName}
+						</h1>
+					</div>
 				</div>
 				<Menubar>
 					<button>
